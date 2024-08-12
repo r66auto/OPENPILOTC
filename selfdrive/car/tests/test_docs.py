6e36ab9a369b2bfecbc0add7afce8958799305bf
Marcello <marcellofuschi1@gmail.com>
2024-08-12 13:16:24 +0000

    Change how car docs diff for PR comment is generated

--- selfdrive/car/tests/test_docs.py
+++ selfdrive/car/tests/test_docs.py
@@ -9,7 +9,6 @@ from openpilot.selfdrive.car.docs import CARS_MD_OUT, CARS_MD_TEMPLATE, generate
 from openpilot.selfdrive.car.docs_definitions import Cable, Column, PartType, Star
 from openpilot.selfdrive.car.honda.values import CAR as HONDA
 from openpilot.selfdrive.car.values import PLATFORMS
-from openpilot.selfdrive.debug.dump_car_docs import dump_car_docs
 from openpilot.selfdrive.debug.print_docs_diff import print_car_docs_diff
 
 
@@ -24,12 +23,6 @@ class TestCarDocs:
       current_cars_md = f.read()
 
     assert generated_cars_md == current_cars_md, "Run selfdrive/car/docs.py to update the compatibility documentation"
-
-  def test_docs_diff(self):
-    dump_path = os.path.join(BASEDIR, "selfdrive", "car", "tests", "cars_dump")
-    dump_car_docs(dump_path)
-    print_car_docs_diff(dump_path)
-    os.remove(dump_path)
 
   def test_duplicate_years(self, subtests):
     make_model_years = defaultdict(list)
